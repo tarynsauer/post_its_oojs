d6058612c8a22e6e51bf0a55ece15c64c1ff7f4a
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-25 12:30:40 -0500

    .

--- spec/postitSpec.js
+++ spec/postitSpec.js
@@ -1,14 +1,66 @@
 describe("board", function() {
-  it("contains a new board", function() {
-      $('body').css("id", "board");
+  
+  beforeEach(function(){
+    $('body').append("<div id='board'></div>");
+  });
+
+  afterEach(function() {
+    $("#board").remove();
+  });
+
+  it("new board sets $elem to the passed selector", function() {
       var newBoard = new Board('#board');
       expect(newBoard.$elem.selector).toBe('#board');
   });
 
-    it("responds to addSticky", function() {
-      $('body').css("id", "board");
+  //   it("responds to addSticky", function() {
+  //     var newBoard = new Board('#board');
+  //     //spyOn(newBoard, "addSticky");
+  //     expect(newBoard.addSticky).toBeDefined();;
+  // });
+
+    it("creates a new sticky", function() {
       var newBoard = new Board('#board');
-      spyOn(newBoard, "addSticky");
-      expect(newBoard.addSticky).toBeDefined();;
+      newBoard.$elem.click();
+      expect(newBoard.$elem.children('ul').length).toEqual(1);
   });
+
+    it("creates a new sticky and deletes it", function() {
+      var newBoard = new Board('#board');
+      newBoard.$elem.click();
+      newBoard.$elem.find('.delete').click();
+      expect(newBoard.$elem.children('ul').length).toBe(0);
+  });
+
 });
+
+// describe("post-it", function() {
+
+//   beforeEach(function(){
+//     $('body').css("id", "board");
+//   });
+
+//   it("is draggable", function() {
+//     var newBoard = new Board('#board');
+//     $('body').click();
+//     console.log($('body').children('ul').first().attr('class'))
+//     expect($('body').children('ul').first().attr('class')).toEqual("post-it ui-draggable");
+//   });
+
+//     it("is editable", function() {
+//     var newBoard = new Board('#board');
+//     $('body').click();
+//     $('.content').click();
+//     expect($('body').children('ul').first().attr('class')) == ("post-it ui-draggable");
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
--- src/post-it.js
+++ src/post-it.js
@@ -2,7 +2,7 @@ var Board = function( selector ) {
   this.$elem = $( selector );
   var self = this;  
 
-  this.$elem.click(function(){
+  this.$elem.dblclick(function(event){
    var newSticky = self.addSticky();
   });
 
@@ -11,37 +11,31 @@ var Board = function( selector ) {
 Board.prototype.addSticky = function() {
   var newSticky = new PostIt();
   this.$elem.append(newSticky.$elem);
-
-
-  $("li span").click(function(event) {
-    event.stopPropagation();
-    $(this).closest(newSticky.$elem).remove();
-   });
 }
 
 var PostIt = function() {
-  this.$elem = $('<ul class="post-it"><li class="header"><span>x</span></li><li class="content" contenteditable="true">This is a post-it</li></ul>');
+  this.$elem = $('<ul class="post-it"><li class="header"><span class="delete">x</span></li><li class="content" contenteditable="true">This is a post-it</li></ul>');
   var postIt ="."+this.$elem.attr('class');
   this.$elem.draggable();
   var self = this;
 
-    $(this.$elem).click(function(event) {
-      event.stopPropagation();
-
-      if ($(this).hasClass('.ui-draggable-dragging')) {
-        return;
-      } else {
-        $(this).draggable("option", "disabled", true);
-        $('.content').attr('contenteditable', 'true');
-        }
-
-      });
-
-  $( ".content" ).blur(function(){
-    $(this.$elem).draggable( 'option', 'disabled', false);
-      console.log($(this));
-    $(this).attr('contenteditable','false');
+  $(this.$elem).dblclick(function(event) {
+    event.stopPropagation();
+    
+    $(this).draggable('option', "disabled", true);
+    $('.content').attr('contenteditable', 'true');
   });
+
+  $(this.$elem).find('.content').blur(function(event){
+    event.stopPropagation();
+    $(this).parent().draggable('option', "disabled", false);
+    $(this).find(".content").attr('contenteditable','false');
+  });
+
+  $(this.$elem).find('.delete').click(function() {
+    console.log(this);
+    $(this).parent().parent().remove();
+   });
 
 };
 
@@ -53,30 +47,30 @@ $(function() {
 });
 
 
-  $(function() {
-    // $('.post-it').draggable();
+  // $(function() {
+  //   // $('.post-it').draggable();
 
-    $(".post-it").click(function() {
-        if ($(this).hasClass('.ui-draggable-dragging')) {
-          return;
-        } else {
-          $(this).draggable("option", "disabled", true);
-          $('.content').attr('contenteditable', 'true');
-        }
+  //   $(".post-it").click(function() {
+  //       if ($(this).hasClass('.ui-draggable-dragging')) {
+  //         return;
+  //       } else {
+  //         $(this).draggable("option", "disabled", true);
+  //         $('.content').attr('contenteditable', 'true');
+  //       }
 
-      });
+  //     });
 
-    $( ".content" ).blur(function(){
-      $('.post-it').draggable( 'option', 'disabled', false);
-      console.log($(this));
-      $(this).attr('contenteditable','false');
-    });
+  //   $( ".content" ).blur(function(){
+  //     $('.post-it').draggable( 'option', 'disabled', false);
+  //     console.log($(this));
+  //     $(this).attr('contenteditable','false');
+  //   });
 
-    $("li span").click(function() {
-      $(this).closest('ul').remove();
-    });
+  //   $("li span").click(function() {
+  //     $(this).closest('ul').remove();
+  //   });
 
-  });
+  // });
 
 
   // $("body").on('click', ".post-it", function() {
